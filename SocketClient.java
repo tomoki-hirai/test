6e0229fff211a653223a7afae778b051552fef16
ttpeanuts <ttpeanuts@gmail.com>
2017-10-02 19:06:27 +0900

    javacommit

---
+++ SocketClient.java
@@ -0,0 +1,10 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+
+class SocketClient {
+    public static void main(String args[]) {
+        System.out.println("hello");
+    }
+}
